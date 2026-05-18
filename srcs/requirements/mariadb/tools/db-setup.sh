9d86a9fc86fa50bfa4d6044b44ff5f1d1a56bf56
Aymen El Arrouji <aymane.arrouji@gmail.com>
2026-05-18 11:29:58 +0100

    nothing

--- srcs/requirements/mariadb/tools/db-setup.sh
+++ srcs/requirements/mariadb/tools/db-setup.sh
@@ -2,11 +2,7 @@
 set -e
 
 if [ ! -d "/var/lib/mysql/mysql" ]; then
-    mysql_install_db \
-        --user=mysql \
-        --datadir=/var/lib/mysql \
-        --skip-test-db \
-        > /dev/null
+    mysql_install_db --user=mysql --datadir=/var/lib/mysql
     mysqld --user=mysql --bootstrap << EOF
 USE mysql;
 FLUSH PRIVILEGES;
