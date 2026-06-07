c9c26f02ec8dbda90ac5ed17b402f81fa9a48429
aymane <aymane.arrouji@gmail.com>
2026-06-07 19:46:33 +0100

    dd

--- srcs/requirements/wordpress/tools/wp_init.sh
+++ srcs/requirements/wordpress/tools/wp_init.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
-cd /var/www/html
+cd /var/www/wordpress
 
 until mysqladmin ping -h mariadb -u${MYSQL_USER} -p${MYSQL_PASSWORD} --silent; do
     echo -e "me:maria are u alive ?\nmaria: no"
     sleep 2
 done
 
-if [ ! -f "/var/www/html/wp-config.php" ]; then
+if [ ! -f "/var/www/wordpress/wp-config.php" ]; then
     echo "Downloading WordPress..."
-    php -d memory_limit=512M $(which wp) core download --allow-root --path=/var/www/html
+    php -d memory_limit=512M $(which wp) core download --allow-root --path=/var/www/wordpress
     wp config create \
         --dbname=${MYSQL_DATABASE} \
         --dbuser=${MYSQL_USER} \
         --dbpass=${MYSQL_PASSWORD} \
         --dbhost=mariadb:3306 \
-        --allow-root --path=/var/www/html
+        --allow-root --path=/var/www/wordpress
 
     wp core install \
         --url=${WP_URL} \
@@ -23,7 +23,7 @@ if [ ! -f "/var/www/html/wp-config.php" ]; then
         --admin_user=${WP_ADMIN_USER} \
         --admin_password=${WP_ADMIN_PASSWORD} \
         --admin_email=${WP_ADMIN_EMAIL} \
-        --allow-root --path=/var/www/html
+        --allow-root --path=/var/www/wordpress
 
     wp user create \
         ${WP_USER} \
@@ -31,15 +31,15 @@ if [ ! -f "/var/www/html/wp-config.php" ]; then
         --user_pass=${WP_PASSWORD} \
         --role=author \
         --allow-root \
-        --path=/var/www/html
+        --path=/var/www/wordpress
 else
     echo "WordPress is already installed."
 fi
 
-chown -R nobody:nobody /var/www/html/wp-content
-chmod -R 775 /var/www/html/wp-content
+chown -R nobody:nobody /var/www/wordpress/wp-content
+chmod -R 775 /var/www/wordpress/wp-content
 
 echo "WordPress setup completed!"
-chown -R nobody:nobody /var/www/html
+chown -R nobody:nobody /var/www/wordpress
 
 exec /usr/sbin/php-fpm84 -F
