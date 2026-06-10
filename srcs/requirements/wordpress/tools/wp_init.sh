12d778ee9159f6eff4f1bdd0c9c4a6d1fb607483
Aymen El Arrouji <aymane.arrouji@gmail.com>
2026-06-10 15:20:01 +0100

    helo

--- srcs/requirements/wordpress/tools/wp_init.sh
+++ srcs/requirements/wordpress/tools/wp_init.sh
@@ -7,7 +7,9 @@ until mysqladmin ping -h mariadb -u${MYSQL_USER} -p${MYSQL_PASSWORD} --silent; d
 done
 
 if [ ! -f "/var/www/wordpress/wp-config.php" ]; then
-    php -d memory_limit=512M $(which wp) core download --allow-root --path=/var/www/wordpress
+    
+    wp core download --allow-root --path=/var/www/wordpress
+    
     wp config create \
         --dbname=${MYSQL_DATABASE} \
         --dbuser=${MYSQL_USER} \
